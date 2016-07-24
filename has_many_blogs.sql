456a17e5fb8162d7402c07d73028e81cf34ef727
Christie R <creindle@hawaii.edu>
2016-07-23 20:00:22 -1000

    Must work on this :O

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -13,7 +13,25 @@ CREATE TABLE users
   username character varying (90) NOT NULL,
   first_name character varying (90) DEFAULT NULL,
   last_name character varying (90) DEFAULT NULL,
-  create_at timestamp with time zone NOT NULL DEFAULT now(),
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  updated_at timestamp with time zone NOT NULL DEFAULT now()
+);
+
+CREATE TABLE posts
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  title character varying (180) DEFAULT NULL,
+  url character varying (510) DEFAULT NULL,
+  content text DEFAULT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  updated_at timestamp with time zone NOT NULL DEFAULT now()
+);
+
+CREATE TABLE comments
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  body character varying (510) DEFAULT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
   updated_at timestamp with time zone NOT NULL DEFAULT now()
 );
 
